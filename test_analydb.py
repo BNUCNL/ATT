923b6393271e4e74edc68eea8267b2a23ecc4934
Taicheng Huang <hitech9310@gmail.com>
2016-02-23 18:48:16 +0800

    main function for analysedb.py

---
+++ test_analydb.py
@@ -0,0 +1,15 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 et:
+
+"test analysedb.py"
+
+from analysedb import *
+
+pklpath = '/nfs/j3/userhome/huangtaicheng/workingdir/program/ATT/data'
+pklfile = 'index_data.pkl'
+
+a = AtlasDB()
+a.read_from_pkl(pklpath, pklfile)
+volume = a.output_data('geo', 'volume')
+mean_alff = a.output_data('rest', 'alff', 'mean')
+zstat_peakcoor = a.output_data('geo', 'peakcoor', 'zstat')
