bba0eb579421a2a1c3586db29645ec2136580689
zhenzonglei <zhenzonglei@gmail.com>
2016-03-05 19:24:16 +0800

    atlas class design

--- atlasdb.py
+++ atlasdb.py
@@ -7,14 +7,17 @@ import nibabel as nib
 import copy
 from scipy import stats
 
-
+from __future__ import division
+import copy
+import cPickle
+import scipy.io as si
+from scipy import stats
 
 
 class UserDefinedException(Exception):
     def __init__(self, str):
         Exception.__init__(self)
         self._str = str
-
 
 def load_img(fimg):
     """
@@ -38,192 +41,150 @@ def load_img(fimg):
     return img
 
 
-def extract_param(targ_img, atlas_img, roi_id, metric):
-    """
-    Parameters
-    ----------
-    targ_img    : a file or a Nifti1Image object for target image
-    atlas_img   : a file or a Nifti1Image object for atlas image
-    roi_id  : a scalar to indicate roi id
-    metric  :  metric used to summarize ROI
-    Returns
-    -------
-    param   : 2-d np.array
-    """
-    targ = load_img(targ_img).get_data()
-    atlas = load_img(atlas_img).get_data()
-
-    if  atlas.shape != targ.shape or atlas.shape != targ.shape[:3]:
-        raise UserDefinedException('Atlas image and target image are not match!')
-
-    if targ.ndim == 3:
-        targ = np.tile(targ, (1, 1))
-
-    if atlas.ndim == 3:
-        atlas = np.tile(atlas, (1, targ.shape[3]))
-
-    NS = targ.shape[3] # number of subjects
-    if metric == 'center' or metric == 'peak':
-        param = np.empty((NS,3))
-    else:
-        param = np.empty(NS)
-    param.fill(np.nan)
-
-    if metric == 'peak':
-        for s in range(NS):
-            d = targ[:, :, :, s] * (atlas[:, :, :, s] == roi_id)
-            param[s, :] = np.unravel_index(d.argmax(), d.shape)
-
-    elif metric == 'center':
-        for s in range(NS):
-            d = targ[:,:,:,s] * (atlas[:,:,:,s] == roi_id)
-            param[s, :]  = np.mean(np.transpose(np.nonzero(d)))
-
-    else: # scalar metric
-        if metric == 'sum' or metric == 'volume':
-            cpu = np.nansum
-        elif metric == 'max':
-            cpu = np.max
-        elif metric == 'min':
-            cpu = np.min
-        elif metric == 'std':
-            cpu = np.nanstd
-        elif metric == 'skewness':
-            cpu = stats.skew
-        elif metric == 'kurtosis':
-            cpu = stats.kurtosis
-        else:
-            cpu = []
-
-        for s in range(NS):
-            d = targ[:,:,:,s]
-            r =  atlas[:,:,:,s] == roi_id
-            param[s] = cpu(d[r], axis=1)
-
-    return param
-
-
-class AtlasInfo(object):
-    def __init__(self,task, contrast, threshold, roi, subj_id, subj_gender):
-        """
-        Parameters
-        ----------
-        task : task name, str
-        contrast : contrast name, str
-        threshold : threshold to define atlas
-        roi  :  roi name and id, a dict
-        subj_id : subject id, a list
-        """
+class Atlas(object):
+    def __init__(self, atlas_img, roi_id, roi_name, task, contrast, threshold, subj_id,subj_gender):
+        self.atlas_img = loadimg(atlas_img)
+        self.roi_name = roi_name
+        self.roi_id = roi_id
         self.task = task
         self.contrast = contrast
-        self.threshold = threshold
-        self.roi = roi
         self.subj_id = subj_id
+        self.subj_gender = subj_gender
 
-    def set_attr(self, name, value):
-        """
-        Parameters
-        ----------
-        name : attribute name
-        value : attribute value
-        """
-        if name == 'task':
-            self.task = value
-        elif name == 'contrast':
-            self.contrast = value
-        elif name  == 'threshold':
-            self.threshold = 'threshold'
-        elif name == 'roi':
-            self.roi = value
-        elif name == 'subj_id':
-            self.subj_id = value
-
-    def get_attr(self, name):
-        """
-        Parameters
-        ----------
-        name : attribute name
-        Returns
-        -------
-        value : attribute value
-        """
-        if name == 'task':
-            value = self.task
-        elif name == 'contrast':
-           value = self.contrast
-        elif name  == 'threshold':
-           value = self.threshold
-        elif name == 'roi':
-            value = self.roi
-        elif name == 'subj_id':
-            value = self.subj_id
-        else:
-             raise UserDefinedException('Wrong attribute name!')
-
-        return value
-
-class AtlasDB(object):
-    def __init__(self, atlas_img, basic, metric={}):
-        """
-        Parameters
-        ----------
-        atlas_img : Nifti1image file or Nifti1image object
-        basic : basic info for atlas, AtlasInfo object
-        data    : data for each roi, dict
-        metric  : metric for each params, dict
-        """
-
-        self.atlas = load_img(atlas_img)
-        self.basic = basic
-        self.metric = metric
         self.data = {}
+        self.data['geo'] = {}
+        self.data['geo']['volume'] = self.volume_meas()
 
-    def import_data(self, targ_img, roi=None, modal='geo', param='volume'):
+    def collect_meas(self, targ_img, metric = 'mean'):
         """
+        Collect measures for atlas
+
         Parameters
         ----------
-        targ_img :  a file or a Nifti1Image object for target image
-        roi : roi info, a (name, id) dict
-        modal : modality, str
-        param : parameter name, str
-        """
-        if roi is None:
-            roi = self.basic.roi
+        targ_img
+        metric
 
-        metric  = self.metric[param]
-        if modal == 'geo':
-            meas = np.empty((len(self.basic.subjid), len(metric)), 3)
-        else:
-             meas = np.empty((len(self.basic.subjid), len(metric)))
-        meas.fill(np.nan)
-
-        for key in roi:
-            for m in range(metric.shape[0]):
-               meas[:,m] =  extract_param(targ_img, self.atlas, roi[key], metric[m])
-
-            self.data[key][modal][param] = meas
-
-    def export_data(self, sessid=None, roi=None, modal='geo', param='volume'):
-        """
-        Parameters
-        ----------
-        sessid : sessid, list
-        roi : roi info a (name, id) dict
-        modal : modality
-        param :  parameter name, str
         Returns
         -------
-        meas : a list
+
         """
 
-        meas = []
-        for key in roi:
-            meas.append(self.data[key][modal][param])
+        candidate_metric = ['mean', 'max', 'min', 'std', 'median', 'skewness', 'kurtosis', 'center', 'peak']
+        if metric not in candidate_metric:
+            raise UserDefinedException('Metric is not supported!')
 
-        return meas
+        targ = load_img(targ_img).get_data()
+        mask = self.atlas_img.get_data()
 
-    def set_attr(self,name, value):
-        pass
+        if  mask.shape != targ.shape or mask.shape != targ.shape[:3]:
+            raise UserDefinedException('Atlas image and target image are not match!')
 
-    def get_attr(self, name, value):
-        pass
+        if targ.ndim == 3:
+            targ = np.tile(targ, (1, 1))
+
+        if mask.ndim == 3:
+            mask = np.tile(mask, (1, targ.shape[3]))
+
+        nSubj = targ.shape[3] # number of subjects
+        nRoi = len(self.roi_id)
+        affine = self.atlas_img.get_affine()
+
+
+        if metric == 'center' or metric == 'peak':
+            param = np.empty((nSubj,nRoi,3))
+        else:
+            param = np.empty(nSubj,nRoi)
+
+        param.fill(np.nan)
+
+        if metric == 'peak':
+            for s in np.arange(nSubj):
+                ijk = np.ones(nRoi,4)
+                for r in np.arange(nRoi):
+                    d = targ[:, :, :, s] * (mask[:, :, :, s] == self.roi_id[r])
+                    ijk[r,0:3] = np.unravel_index(d.argmax(), d.shape)
+
+                 param[s,:,:] = np.dot(affine,ijk.T)[0:3,:].T
+
+        elif metric == 'center':
+            for s in np.arange(nSubj):
+                ijk = np.ones(nRoi,4)
+                for r in np.arange(nRoi):
+                    d = targ[:,:,:,s] * (mask[:,:,:,s] == self.roi_id[r])
+                    ijk[r,0:3] = np.mean(np.transpose(np.nonzero(d)))
+
+                param[s,:,:]  = np.dot(affine, ijk.T)[0:3,:].T
+
+        else: # scalar metric
+            if metric == 'sum':
+                meter = np.nansum
+            elif metric == 'max':
+                meter = np.max
+            elif metric == 'min':
+                meter = np.min
+            elif metric == 'std':
+                meter = np.nanstd
+            elif metric == 'skewness':
+                meter = stats.skew
+            elif metric == 'kurtosis':
+                meter = stats.kurtosis
+            else:
+                meter = []
+
+            for s in np.arange(nSubj):
+                for r in np.arange(nRoi):
+                    d = targ[:,:,:,s]
+                    r = mask[:,:,:,s] == self.roi_id[r]
+                    param[s,r] = meter(d[r], axis=1)
+
+        return param
+
+    def volume_meas(self):
+        mask = self.atlas_img.get_data()
+
+        # extend 3d mask to 4d
+        if mask.ndim == 3:
+            mask = np.tile(mask, (1, 1))
+
+        # number of subjects
+        nSubj = mask.shape[3]
+        nRoi = len(self.roi_id)
+
+        vol = np.zeros((nSubj,nRoi))
+        # iterate for subject and roi
+        for s in np.arange(nSubj):
+                for r in np.arange(nRoi):
+                    vol[s,r] = np.sum(mask[:,:,:,s] == self.roi_id[r])
+
+        res = self.atlas_img.header.get_zooms()
+        return vol*np.prod(res)
+
+    def save_to_pkl(self, path, filename):
+        """
+        Parameters
+        ----------
+        save data in os.path.join(path, filename) with type of .pkl
+        path    parent path
+        filename    filename .pkl
+        Returns
+        A .pkl file,which can be loaded by cPickle
+        -------
+        """
+        if hasattr(self, 'data'):
+            with open(os.path.join(path, filename), 'wb') as output:
+                cPickle.dump(self.data, output, -1)
+
+    def save_to_mat(self, path, filename):
+        """
+        Parameters
+        ----------
+        save data in os.path.join(path, filename) with type of .mat
+        path
+        filename
+        Returns
+        -------
+        A .mat file,which can be loaded by matlab
+        """
+        if hasattr(self, 'data'):
+            si.savemat(os.path.join(path, filename), mdict = self.data)
+
