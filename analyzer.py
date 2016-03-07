0def8547fedea2fe846c9f896fcd71fde6b12bf9
zhenzonglei <zhenzonglei@gmail.com>
2016-03-07 14:13:48 +0800

    NF: first implementation of Analyzer

--- analyzer.py
+++ analyzer.py
@@ -1,11 +1,10 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 et:
 
-import os
 import numpy as np
+import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
-import seaborn as sns
-
+import scipy.spatial.distance.cdist as cdist
 
 
 class Analyzer(object):
@@ -15,7 +14,7 @@ class Analyzer(object):
         self.roi_name = roi_name
         self.feature_name  = feature_name
 
-    def feature_desp(self, meth, figure):
+    def feature_description(self, feat_sel=None, figure=True):
         """
         feature description and plot
         Parameters
@@ -27,24 +26,30 @@ class Analyzer(object):
 
         """
 
-        fmean = np.nanmean(self.meas)
-        fstd  = np.nanstd(self.meas)
+        nFeat = len(self.feature_name) # number of feature
+        if feat_sel is None:
+            feat_sel = np.arange(nFeat)
 
-        nSubj = self.meas.shape[0] # number of subjects
-        nRoi = len(self.roi_id) # number of ROI
-        nFeat =  len(self.feature_name) # number of feature
-        for f in np.arange(nFeat):
-            ## plot for feature
-            roi_name = self.feature_name[np.floor(np.divide(f, nROI))]
-            feat_name = self.roi_name[np.mod(f, nROI)]
+        fmean = np.nanmean(self.meas[:, feat_sel])
+        fstd  = np.nanstd(self.meas[:, feat_sel])
 
+        if figure:
+            nRoi = len(self.roi_id) # number of ROI
+            for f in feat_sel:
+                roi_name = self.feature_name[np.floor(np.divide(f, nRoi))]
+                feat_name = self.roi_name[np.mod(f, nRoi)]
 
+                plt.hist(np.isnan(self.meas[:, f]))
+                plt.xlabel(roi_name+'-'+feat_name)
+                plt.ylabel('Probability')
+                plt.title('Histogram for ROI feature')
+                plt.show()
 
         return fmean, fstd
 
 
 
-    def feature_relation(self, meth, figure):
+    def feature_relation(self, feat_sel=None, figure=True):
         """
 
         Parameters
@@ -56,13 +61,22 @@ class Analyzer(object):
         -------
 
         """
+        nFeat = len(self.feature_name) # number of feature
+        if feat_sel is None:
+            feat_sel = np.arange(nFeat)
 
-       feat_corr = np.corrcoef(self.meas)
+        feat_corr = np.corrcoef(self.meas[:, feat_sel])
 
-       # plot feat_corr
+        # plot feat_corr
+        if figure:
+            fig, ax = plt.subplots()
+            heatmap = ax.pcolor(feat_corr)
+            plt.show()
+
+        return feat_corr
 
 
-    def behavior_predict1(self, meth, behavior):
+    def behavior_predict1(self, feat_sel, behavior):
         """
         Univariate predict
         Parameters
@@ -74,6 +88,20 @@ class Analyzer(object):
         -------
 
         """
+
+        nFeat = len(self.feature_name) # number of feature
+        if feat_sel is None:
+            feat_sel = np.arange(nFeat)
+
+        feat_beh_corr = 1 - cdist(self.meas[:, feat_sel], behavior)
+
+
+        if figure:
+            fig, ax = plt.subplots()
+            heatmap = ax.pcolor(feat_beh_corr])
+            plt.show()
+
+        return feat_beh_corr
 
 
 
@@ -89,6 +117,7 @@ class Analyzer(object):
         -------
 
         """
+        pass
 
 
 
@@ -100,18 +129,25 @@ class Analyzer(object):
         -------
 
         """
+        pass
 
 
 
-    def outlier_remove(self, meth, figure):
+    def outlier_remove(self, outlier_sel):
         """
         remove outlier
         Parameters
         ----------
-        meth
-        figure
+        outlier_sel: outlier index, 1-d np.array
 
         Returns
         -------
 
         """
+        nSubj = self.meas.shape[0] # number of subjects
+        good_subj = np.ones(nSubj, dtype=bool)
+        good_subj[outlier_sel] = False
+        self.meas = self.meas[good_subj, :]
+
+
+        return self.meas
