d0ac42da75b9ebb8206af7b1538402fff969ecae
zhenzonglei <zhenzonglei@gmail.com>
2016-03-07 12:22:04 +0800

    NF: interface design for class Analyzer

--- analyzer.py
+++ analyzer.py
@@ -27,33 +27,91 @@ class Analyzer(object):
 
         """
 
+        fmean = np.nanmean(self.meas)
+        fstd  = np.nanstd(self.meas)
+
+        nSubj = self.meas.shape[0] # number of subjects
+        nRoi = len(self.roi_id) # number of ROI
+        nFeat =  len(self.feature_name) # number of feature
+        for f in np.arange(nFeat):
+            ## plot for feature
+            roi_name = self.feature_name[np.floor(np.divide(f, nROI))]
+            feat_name = self.roi_name[np.mod(f, nROI)]
+
+
+
+        return fmean, fstd
 
 
 
     def feature_relation(self, meth, figure):
+        """
+
+        Parameters
+        ----------
+        meth
+        figure
+
+        Returns
+        -------
+
+        """
+
+       feat_corr = np.corrcoef(self.meas)
+
+       # plot feat_corr
+
+
+    def behavior_predict1(self, meth, behavior):
+        """
+        Univariate predict
+        Parameters
+        ----------
+        meth
+        behavior
+
+        Returns
+        -------
+
+        """
 
 
 
-    def behavior_predict(self, behavior):
+    def behavior_predict2(self, meth, behavior):
+        """
+        Multivariate predict
+        Parameters
+        ----------
+        meth
+        behavior
+
+        Returns
+        -------
+
+        """
 
 
 
 
     def topymvpa(self):
+        """
+        Generate pymvpa dataset
+        Returns
+        -------
+
+        """
 
 
 
-    def outlier_detect(self, meth, figure):
+    def outlier_remove(self, meth, figure):
+        """
+        remove outlier
+        Parameters
+        ----------
+        meth
+        figure
 
+        Returns
+        -------
 
-
-
-
-
-
-
-
-
-
-
-
+        """
