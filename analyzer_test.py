8290999d7e29e88997539f5fae4e74be018b944c
zhenzonglei <zhenzonglei@gmail.com>
2016-03-08 13:13:52 +0800

    ENH: full atlas_test

--- analyzer_test.py
+++ analyzer_test.py
@@ -1,101 +1,29 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 et:
 
-"test analysedb.py"
 
-from analysedb import *
-
-pklpath = '/nfs/j3/userhome/huangtaicheng/workingdir/program/ATT/data'
-pklfile = 'index_data.pkl'
-areaname = ['rV3', 'lV3', 'rMT', 'lMT']
-areanum = [1,2,3,4]
+from analyzer import *
+import os
+import cPickle
 
 
-atlas = AtlasDB()
-atlas.read_from_pkl(pklpath, pklfile)
+data_path = './data'
+mt_data_path = os.path.join(data_path, 'mt.pkl')
 
-# volume
-volume = atlas.output_data('geo', 'volume')
-# zvalue
-mean_zstat = atlas.output_data('act', 'zstat', 'mean')
-peak_zstat = atlas.output_data('act', 'zstat', 'max')
-std_zstat = atlas.output_data('act', 'zstat', 'std')
-# psc value
-mean_psc = atlas.output_data('act', 'psc', 'mean')
-peak_psc = atlas.output_data('act', 'psc', 'max')
-std_psc = atlas.output_data('act', 'psc', 'std')
-# alff
-mean_alff = atlas.output_data('rest', 'alff', 'mean')
-peak_alff = atlas.output_data('rest', 'alff', 'max')
-std_alff = atlas.output_data('rest', 'alff', 'std')
-# falff
-mean_falff = atlas.output_data('rest', 'falff', 'mean')
-peak_falff = atlas.output_data('rest', 'falff', 'max')
-std_falff = atlas.output_data('rest', 'falff', 'std')
-# reho
-mean_reho = atlas.output_data('rest', 'reho', 'mean')
-peak_reho = atlas.output_data('rest', 'reho', 'max')
-std_reho = atlas.output_data('rest', 'reho', 'std')
-# peakcoor
-zstat_peakcoor = atlas.output_data('geo', 'peakcoor', 'zstat')
-psc_peakcoor = atlas.output_data('geo', 'peakcoor', 'psc')
-alff_peakcoor = atlas.output_data('geo', 'peakcoor', 'alff')
-falff_peakcoor = atlas.output_data('geo', 'peakcoor', 'falff')
-reho_peakcoor = atlas.output_data('geo', 'peakcoor', 'reho')
+mt_file = open(mt_data_path, 'r')
+data = cPickle.load(mt_file)
+file.close()
 
-#----------------volume------------------------------------
-atlasdes_volume = AtlasDescribe(volume, areaname)
-#------------------------------
-"""
-Calculate "percent subject" and "number of subject"
-"""
-existnum = np.empty(len(areaname))
-existperc = np.empty(len(areaname))
-for i in range(len(areaname)):
-    atlasdes_volume.subjexist(areaname[i])
-    existnum[i] = atlasdes_volume.existnum
-    existperc[i] = atlasdes_volume.existperc
-#--------------------------------
-volumemean = []
-volumestd = []
-volumeli = []
-for areai in areaname:
-    atlasdes_volume.paradescrib(areai)
-    volumemean.append(atlasdes_volume.datamean)
-    volumestd.append(atlasdes_volume.datastd)
-volumemean = np.array(volumemean)
-volumestd = np.array(volumestd)
+roi_name = data.roi_name
+subj_id = data.subj_id
+roi_name = data.roi_name
+meas = data.meas
+feature_name = data.feature_name
 
-for i in range(0, len(areaname)-1, 2):
-    atlasdes_volume.calhemLi(areaname[i][1:])
-    volumeli.append(atlasdes_volume.livalue)
-volumeli = np.array(volumeli)
-#------------------------------------------------------------
-
-atlasdes = AtlasDescribe(mean_alff, areaname)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-atlasdes.subjexist('lMT')
-atlasdes.paradescrib('lMT')
-atlasdes.calhemLi('V3')
-
+mt_analyzer = Analyzer(meas, subj_id, roi_name, feature_name)
+mt_analyzer.feature_description()
+mt_analyzer.feature_relation()
+mt_analyzer.behavior_predict1(beh_meas)
 
 
 
--- atlas_test.py
+++ atlas_test.py
@@ -4,18 +4,9 @@
 
 import os
 import pandas as pd
-from atlasdb import *
+from atlas import *
 import nibabel as nib
-
-
-data_path = './data'
-zstat_img_path = os.path.join(data_path, 'zstat.nii.gz')
-zstat_img = nib.load(zstat_img_path)
-
-
-mask_img_file = os.path.join(data_path, 'mt_z5.0.nii.gz')
-mask_img_file = os.path.join(data_path, 'mt.nii.gz')
-
+import cPickle
 
 roi_name = ['rV3', 'lV3', 'rMT', 'lMT']
 roi_id = [1, 2, 3, 4]
@@ -24,15 +15,39 @@ contrast = 'motion-fix'
 threshold = 5.0
 
 
+data_path = './data'
+
 subj_gender = pd.read_csv(os.path.join(data_path, 'act_sex.csv'))['gender'].tolist()
-subj_id = open(os.path.join(data_path,'actID'),'rU').read().splitlines()
+subj_id = open(os.path.join(data_path, 'actID'), 'rU').read().splitlines()
 
 
+mask_img_file = os.path.join(data_path, 'mt.nii.gz')
 mt_atlas = Atlas(mask_img_file, roi_id, roi_name, task, contrast, threshold, subj_id, subj_gender)
-zstat_mean = mt_atlas.collect_scalar_meas(zstat_img, 'mean')
-zstat_peak_coords = mt_atlas.collect_geometry_meas(zstat_img, 'peak')
 
 
+meas_name = ['zstat.nii.gz', 'falff.nii.gz']
+meas_mean = np.array([]).reshape(len(subj_id), len(roi_id))
+meas_peak_coords = np.array([]).reshape(len(subj_id), len(roi_id), 3)
+for m in meas_name:
+    meas_img_path = os.path.join(data_path, m)
+    meas_img = nib.load(meas_img_path)
+
+    mean_value = mt_atlas.collect_scalar_meas(meas_img, 'mean')
+    meas_mean = np.hstack((meas_mean, mean_value))
+
+    peak_coords = mt_atlas.collect_geometry_meas(meas_img, 'peak')
+    meas_peak_coords = np.hstack((meas_peak_coords, peak_coords))
+    print m
+
+data = dict.fromkeys(['meas', 'subj_id', 'roi_name', 'feat_name'], None)
+data['meas'] = meas_mean
+data['subj_id'] = subj_id
+data['roi_name'] = roi_id
+data['feat_name'] = ['act-mean', 'fallf-mean']
+
+
+with open(os.path.join(data_path, 'zstat-falff'), 'wb') as out_file:
+        cPickle.dump(data, out_file, -1)
 
 
 
