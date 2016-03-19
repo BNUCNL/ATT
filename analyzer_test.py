ef57a89ecf048a04540cd46f69765282af35cbc5
zhenzonglei <zhenzonglei@gmail.com>
2016-03-19 14:18:14 +0800

    EHN: contrast,t,p are supported for behavior predict2

--- analyzer_test.py
+++ analyzer_test.py
@@ -14,12 +14,12 @@ mt_file.close()
 
 
 # prep inputs
-meas = data['meas_peak_coords'][:100, :]
+meas = data['meas_mean'][:100, :]
 subj_id = data['subj_id']
 roi_name = data['roi_name']
 meas_name = data['meas_name']
 subj_gender = data['subj_gender']
-meas_type = 'geometry'
+meas_type = 'scalar'
 
 # generate an analyzer
 mt_analyzer = Analyzer(meas, meas_type, meas_name, roi_name, subj_id, subj_gender)
@@ -27,24 +27,26 @@ mt_analyzer = Analyzer(meas, meas_type, meas_name, roi_name, subj_id, subj_gende
 # merge data from two hemisphere
 mt_analyzer.hemi_merge()
 
-feat_sel = np.arange(mt_analyzer.meas.shape[1])
+sel = np.arange(mt_analyzer.meas.shape[1])
+
+
 # description for each features, and save stats
-feat_stats = mt_analyzer.feature_description(feat_sel, figure=False)
+feat_stats = mt_analyzer.feature_description(feat_sel=sel , figure=False)
 np.savetxt(os.path.join(data_path, 'feat_stats.txt'), feat_stats, fmt='%.4f')
 
 # description for the relation among each pair of feature
-feat_corr, feat_pval, n_sample = mt_analyzer.feature_relation(feat_sel, figure=False)
+feat_corr, feat_pval, n_sample = mt_analyzer.feature_relation(feat_sel=sel, figure=False)
 
 # uni-variate  behavior predict
 beh_meas = np.mean(meas, axis=1) # fake behavior use mean brain meas
-beh_corr, beh_pval, beh_nsamp = mt_analyzer.behavior_predict1(beh_meas, ['fakeBeh'], feat_sel, figure=False)
+beh_corr, beh_pval, beh_nsamp = mt_analyzer.behavior_predict1(beh_meas, ['fakeBeh'], feat_sel=sel, figure=False)
 
 # multivariate behavior predict
 beh_meas = np.random.randn(meas.shape[0], 1)
-reg_stats = mt_analyzer.behavior_predict2(beh_meas, ['RandBeh'], feat_sel, figure=True)
+slop_stats, r2, dof = mt_analyzer.behavior_predict2(beh_meas, ['RandBeh'], feat_sel=sel, figure=False)
 
 # calculate hemisphere asymmetry
-li_stats = mt_analyzer.hemi_asymmetry(feat_sel, figure=True)
+li_stats = mt_analyzer.hemi_asymmetry(feat_sel=sel, figure=False)
 
 # calculate gender difference
-gd_stats = mt_analyzer.gender_diff(feat_sel, figure=True)
+gd_stats = mt_analyzer.gender_diff(feat_sel=sel, figure=False)
