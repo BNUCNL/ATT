208b35bc451d07c4613e5a277ab9032cc13913ae
helloTC <hitech9310@gmail.com>
2016-11-12 10:09:55 +0800

    seperate merge hemispheres and outlier removed from classes

--- corefunc/analysebase.py
+++ corefunc/analysebase.py
@@ -21,8 +21,61 @@ _plot_bar = _figfactory.createfactory('bar')
 _plot_hist = _figfactory.createfactory('hist')
 _plot_hierarchy = _figfactory.createfactory('hierarchy')
 
+
+def dataprocess(data, hemiident, outlier_method = None, outlier_range = [-3,3], mergehemi = False):
+    """
+    Pipline to merge hemisphere and do outlier removed.
+    ---------------------------------------------------------
+    Parameters:
+        data: raw data, 
+        hemiident: identify to point hemisphere. Should be a bool expression.
+        outlier_method: 'iqr' or 'std' or 'abs'. By default is None
+        outlier_range: outlier standard threshold
+        mergehemi: merge hemisphere or not. By default is False
+    Output:
+        n_removed: outlier_numbers
+        residue_data: output data
+
+    Example:
+        >>> a = np.array([[1,2,3,4],[5,6,7,8]])
+        >>> b = array([True,True,False,False], dtype=bool)
+        >>> n_removed, residue_data = dataprocess(a,b)
+    """
+    hemiident = np.array(hemiident).astype('bool')
+    if not len(hemiident[hemiident]) == len(hemiident[~hemiident]):
+        raise Exception("length of left data should equal to right data")
+
+    if data.ndim == 1:
+        data = data[...,np.newaxis]
+    if data.ndim == 2:
+        data = data[...,np.newaxis]
+    if data.ndim != 3:
+        raise Exception('data dimensions should be 2 or 3!')
+    if mergehemi:
+        n_removed = np.empty((data.shape[1]/2, data.shape[2]))
+        data_comb = np.empty((data.shape[0], data.shape[1]/2, data.shape[2]))
+        data_removed = np.empty((data.shape[0], data.shape[1]/2, data.shape[2]))
+        for i in range(data.shape[0]):
+            for j in range(data.shape[2]):
+                data_comb[i,:,j] = tools.hemi_merge(data[i,hemiident,j], data[i,~hemiident,j])
+        for i in range(data.shape[1]/2):
+            for j in range(data.shape[2]):
+                n_removed[i,j], data_removed[:,i,j] = tools.removeoutlier(data_comb[:,i,j], meth = outlier_method, thr = outlier_range)
+    else:
+        data_comb = data
+        n_removed = np.empty((data.shape[1], data.shape[2]))
+        data_removed = np.zeros_like(data)
+        for i in range(data.shape[1]):
+            for j in range(data.shape[2]):
+                n_removed[i,j], data_removed[:,i,j] = tools.removeoutlier(data_comb[:,i,j], meth = outlier_method, thr = outlier_range)
+    if n_removed.shape[-1] == 1:
+        n_removed = n_removed[...,0]
+    if data_removed.shape[-1] == 1:
+        data_removed = data_removed[...,0]
+    return n_removed, data_removed
+
 class FeatureDescription(object):
-    def __init__(self, meas, regions, outlier_method = 'iqr', outlier_range = [-3, 3], mergehemi = False, figure = False):
+    def __init__(self, meas, hemiident, regions, outlier_method = 'iqr', outlier_range = [-3, 3], mergehemi = False, figure = False):
         """
         Parameters:
         -------------
@@ -31,6 +84,7 @@ class FeatureDescription(object):
               meas are matrix of (nsubject)x(nregions)
               each feature should has order r/l or l/r
               Therefore feature classification is nfeature/2
+        hemiident: identify to point hemispheres
         regions: regions contain in meas.
                  Note that if you would like to merge hemisphere, regions should be regions have no hemispheric identity
         outlier_method: remove outlier criterion, 'iqr' or 'std' or 'abs'
@@ -40,23 +94,10 @@ class FeatureDescription(object):
         """
         if isinstance(meas, list):
             meas = np.array(meas)
-        if mergehemi:
-            data = np.empty((meas.shape[0], meas.shape[1]/2))
-            data_removed = np.empty((meas.shape[0], meas.shape[1]/2))
-            n_removed = np.empty(meas.shape[1]/2)
-            for i in range(data.shape[1]):
-                data[:,i] = tools.hemi_merge(meas[:,2*i], meas[:,2*i+1])
-            feat_stats = np.empty((5, meas.shape[1]/2))
-            for i in range(data.shape[1]):
-                n_removed[i], data_removed[:,i] = tools.removeoutlier(data[:,i], meth = outlier_method, thr = outlier_range)
-        else:
-            data = meas
-            feat_stats = np.empty((5, meas.shape[1]))
-            data_removed = np.empty((meas.shape[0], meas.shape[1]))
-            n_removed = np.empty(meas.shape[1])
-            for i in range(data.shape[1]):
-                n_removed[i], data_removed[:,i] = tools.removeoutlier(data[:,i], meth = outlier_method, thr = outlier_range)
-        
+
+        n_removed, data_removed = dataprocess(meas,hemiident)
+
+        feat_stats = np.empty((5, data_removed.shape[1]))
         self.regions = regions
         self.nsubj = meas.shape[0]
         self.figure = figure
@@ -84,34 +125,22 @@ class FeatureDescription(object):
 
 class FeatureRelation(object):
     # Class for feature relationship
-    def __init__(self, meas, keys, outlier_method = 'iqr', outlier_range = [-3, 3], mergehemi = False, figure = False):
+    def __init__(self, meas, hemiident, regions, outlier_method = 'iqr', outlier_range = [-3, 3], mergehemi = False, figure = False):
         """
         Parameters:
             meas: raw data
-            keys: keys(or label) of raw data
+            hemiident: identify to point hemispheres
+            regions: regions(or label) of raw data
             outlier_method: outlier analysis
             
              
         """
         if isinstance(meas, list):
             meas = np.array(meas)
-        # Merge hemispheres and remove outlier
-        if mergehemi:
-            data = np.empty((meas.shape[0], meas.shape[1]/2))
-            data_removed = np.empty((meas.shape[0], meas.shape[1]/2))
-            n_removed = np.empty(meas.shape[1]/2)
-            for i in range(data.shape[1]):
-                data[:,i] = tools.hemi_merge(meas[:,2*i], meas[:,2*i+1])
-            for i in range(data.shape[1]):
-                n_removed[i], data_removed[:,i] = tools.removeoutlier(data[:,i], meth = outlier_method, thr = outlier_range)
-        else:
-            data = meas
-            data_removed = np.empty((meas.shape[0], meas.shape[1]))
-            n_removed = np.empty(meas.shape[1])
-            for i in range(data.shape[1]):
-                n_removed[i], data_removed[:,i] = tools.removeoutlier(data[:,i], meth = outlier_method, thr = outlier_range)   
 
-        self.keys = keys
+        n_removed, data_removed = dataprocess(meas, hemiident)
+
+        self.regions = regions
         self.figure = figure
         self.data_removed = data_removed
         self.n_removed = n_removed 
@@ -145,11 +174,11 @@ class FeatureRelation(object):
         elif self.data_removed.shape[1] == 2:
             corr, pval = calfunc(tools.listwise_clean(self.data_removed)[:,0], tools.listwise_clean(self.data_removed)[:,1])
             if self.figure:
-               _plot_corr(self.data_removed[:,0], self.data_removed[:,1], self.keys, method)  
+               _plot_corr(self.data_removed[:,0], self.data_removed[:,1], self.regions, method)  
         else:
             corr, pval = tools.calwithincorr(tools.listwise_clean(self.data_removed), method)
             if self.figure:
-                _plot_mat(corr, self.keys, self.keys)
+                _plot_mat(corr, self.regions, self.regions)
         return corr, pval
 
     def feature_prediction2(self, estimator):
@@ -194,11 +223,11 @@ class FeatureRelation(object):
                 r2[1], beta[:,1], tval[i,1], tpval[i,1], f[1], fpval[1] = tools.lin_betafit(estimator, measdata2[:,1:], measdata2[:,0], c)
         if self.figure:
             if self.mergehemi:
-                xlbl = self.keys[1:]
+                xlbl = self.regons[1:]
                 _plot_bar(beta, 'Scaled beta', xlbl, 'beta values', ['beta values'])
             else:
-                xlbl1 = self.keys[2::2]
-                xlbl2 = self.keys[3::2]
+                xlbl1 = self.regions[2::2]
+                xlbl2 = self.regions[3::2]
                 _plot_bar(beta[:,0], 'Scaled beta', xlbl1, 'beta values', ['beta values'])
                 _plot_bar(beta[:,1], 'Scaled beta', xlbl2, 'beta values', ['beta values'])
         return r2, beta, tval, tpval, f, fpval
@@ -235,22 +264,23 @@ class FeatureRelation(object):
             scores[1], n_scores[:,1], pvalues[1] = tools.permutation_cross_validation(estimator, measdata2[:, 1:], measdata2[:,0], n_fold, isshuffle, cvmeth, score_type, n_perm)
         if self.figure:
             if self.mergehemi:
-                xlbl = self.keys
+                xlbl = self.regions
                 _plot_hist(n_scores, xlbl, scores, pvalues)
             else:
-                xlbl1 = self.keys[2::2]
-                xlbl2 = self.keys[3::2]
+                xlbl1 = self.regions[2::2]
+                xlbl2 = self.regions[3::2]
                 _plot_hist(n_scores[:,0], xlbl1, scores[0], pvalues[0])
                 _plot_hist(n_scores[:,1], xlbl2, scores[1], pvalues[1])
         return scores, n_scores, pvalues
 
 class ComPatternMap(object):
-    def __init__(self, data, regions, outlier_method = None, outlier_range = [-3, 3], mergehemi = False, figure = False):
+    def __init__(self, data, hemiident, regions, outlier_method = None, outlier_range = [-3, 3], mergehemi = False, figure = False):
         """
         Parameters:
             data: raw data. It could be 2D or 3D data.
                   2D data is activation data. Which is the form of nsubj*regions
                   3D data is roi resting data. Which is the form of timeseries*regions*nsubj
+            hemiident: identify to point hemisphere, should be a bool expression
             regions: region names
             outlier_method: criterion of outlier removed, 'iqr' or 'std' or 'abs'
             outlier_range: outlier range
@@ -259,43 +289,9 @@ class ComPatternMap(object):
         """
         if not isinstance(regions, list):
             regions = regions.tolist()
-        if mergehemi:
-            if data.ndim == 2:
-                n_removed = np.empty(data.shape[1]/2)
-                data_comb = np.empty((data.shape[0], data.shape[1]/2))
-                data_removed = np.empty((data.shape[0], data.shape[1]/2))      
-                for i in range(data_comb.shape[1]):
-                    data_comb[:,i] = tools.hemi_merge(data[:,2*i], data[:,2*i+1])
-                for i in range(data_comb.shape[1]):
-                    n_removed[i], data_removed[:,i] = tools.removeoutlier(data_comb[:,i], meth = outlier_method, thr = outlier_range)
-            elif data.ndim == 3:
-                n_removed = np.empty((data.shape[1]/2, data.shape[2]))
-                data_comb = np.empty((data.shape[0], data.shape[1]/2, data.shape[2]))
-                data_removed = np.empty((data.shape[0], data.shape[1]/2, data.shape[2]))
-                for i in range(data_comb.shape[1]):
-                    for j in range(data_comb.shape[2]):
-                        data_comb[:,i,j] = tools.hemi_merge(data[:,2*i,j], data[:,2*i+1,j])
-                for i in range(data_comb.shape[1]):
-                    for j in range(data_comb.shape[2]):
-                        n_removed[i,j], data_removed[:,i,j] = tools.removeoutlier(data_comb[:,i,j], meth = outlier_method, thr = outlier_range)
-            else:
-                raise Exception('Support 2D or 3D data now')
-        else:
-            data_comb = data
-            if data.ndim == 2:
-                n_removed = np.empty(data.shape[1])
-                data_removed = np.empty((data_comb.shape[0], data_comb.shape[1]))
-                for i in range(data_comb.shape[1]):  
-                    n_removed[i], data_removed[:,i] = tools.removeoutlier(data_comb[:,i], meth = outlier_method, thr = outlier_range)
-            elif data.ndim == 3:
-                n_removed = np.empty((data.shape[1], data.shape[2]))
-                data_removed = np.empty((data_comb.shape[0], data_comb.shape[1], data_comb.shape[2]))
-                for i in range(data_comb.shape[1]):
-                    for j in range(data_comb.shape[2]):
-                        n_removed[i,j], data_removed[:,i,j] = tools.removeoutlier(data_comb[:,i,j], meth = outlier_method, thr = outlier_range)
-            else:
-                raise Exception('Support 2D or 3D data now')
-  
+        
+        n_removed, data_removed = dataprocess(data, hemiident)
+        
         self.regions = regions
         self.data_removed = data_removed
         self.n_removed = n_removed
