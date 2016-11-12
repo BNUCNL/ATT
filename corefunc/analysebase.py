2b2b8f200597bc3ddf2e9e63b3146a4ff48cdfb4
helloTC <hitech9310@gmail.com>
2016-11-12 10:16:53 +0800

    rename dataprocess into data_preprocess

--- corefunc/analysebase.py
+++ corefunc/analysebase.py
@@ -22,7 +22,7 @@ _plot_hist = _figfactory.createfactory('hist')
 _plot_hierarchy = _figfactory.createfactory('hierarchy')
 
 
-def dataprocess(data, hemiident, outlier_method = None, outlier_range = [-3,3], mergehemi = False):
+def data_preprocess(data, hemiident, outlier_method = None, outlier_range = [-3,3], mergehemi = False):
     """
     Pipline to merge hemisphere and do outlier removed.
     ---------------------------------------------------------
@@ -95,7 +95,7 @@ class FeatureDescription(object):
         if isinstance(meas, list):
             meas = np.array(meas)
 
-        n_removed, data_removed = dataprocess(meas,hemiident, outlier_method, outlier_range, mergehemi)
+        n_removed, data_removed = data_preprocess(meas,hemiident, outlier_method, outlier_range, mergehemi)
 
         feat_stats = np.empty((5, data_removed.shape[1]))
         self.regions = regions
@@ -138,7 +138,7 @@ class FeatureRelation(object):
         if isinstance(meas, list):
             meas = np.array(meas)
 
-        n_removed, data_removed = dataprocess(meas, hemiident, outlier_method, outlier_range, mergehemi)
+        n_removed, data_removed = data_preprocess(meas, hemiident, outlier_method, outlier_range, mergehemi)
 
         self.regions = regions
         self.figure = figure
@@ -290,7 +290,7 @@ class ComPatternMap(object):
         if not isinstance(regions, list):
             regions = regions.tolist()
         
-        n_removed, data_removed = dataprocess(data, hemiident, outlier_method, outlier_range, mergehemi)
+        n_removed, data_removed = data_preprocess(data, hemiident, outlier_method, outlier_range, mergehemi)
         
         self.regions = regions
         self.data_removed = data_removed
