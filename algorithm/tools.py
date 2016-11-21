f6f2f4ab5f8abcc0bcf24a3c5002693fa1ee1fd5
hellotc <taicheng_huang@sina.cn>
2016-11-21 04:01:25 -0800

    Feature Scaling

--- algorithm/tools.py
+++ algorithm/tools.py
@@ -388,9 +388,73 @@ def permutation_cross_validation(estimator, X, y, n_fold=3, isshuffle = True, cv
     score, permutation_scores, pvalues = cross_validation.permutation_test_score(estimator, X, y, scoring = score_type, cv = cvmethod, n_permutations = n_perm)
     return score, permutation_scores, pvalues
 
+class FeatureScale(object):
+    """
+    A class for feature scaling
+    -------------------------------
+    Parameters:
+        data: raw array data
+    """
+    def __init__(self, data):
+        if not isinstance(data, np.ndarray):
+            raise Exception("Data should be np.ndarray data")
+        self._data = data
+        self._shape = data.shape
+    def rescaling(self):
+        """
+        Rescaling data
+        x' = (x-min(x))/(max(x)-min(x))
+        --------------------------
+        Example:
+            >>> featCls = tools.FeatureScale(data)
+            >>> outdata = featCls.rescaling()
+        """
+        flattendata = self._data.flatten().tolist()
+        mindata = np.min(self._data)
+        maxdata = np.max(self._data)
+        delta = maxdata - mindata
+        flattenoutput = [(x-mindata)/delta if x!=0 else 0 for x in flattendata]
+        return np.reshape(np.array(flattenoutput), self._shape)
+
+    def standardization(self):
+        """
+        Feature standardization
+        x' = (x-mean(x))/std(x)
+        -------------------------
+        Example:
+            >>> featCls = tools.FeatureScale(data)
+            >>> outdata = featCls.standardization()
+        """
+        flattendata = self._data.flatten().tolist()
+        meandata = np.mean(self._data[self._data!=0])
+        stddata = np.std(self._data[self._data!=0])
+        flattenoutput = [(x-meandata)/stddata if x!=0 else 0 for x in flattendata]
+        return np.reshape(np.array(flattenoutput), self._shape)
+
+    def scale_unit_length(self, para = 'L1'):
+        """
+        Scaling to unit length
+        x' = x/||x||
+        -----------------------------
+        Parameters:
+            para: 'L1', L1 norm. ||x|| = sum(abs(x))
+                  'L2', L2 norm. ||x|| = sum(x**2)
+        Example:
+            >>> featCls = tools.FeatureScale(data)
+            >>> outdata = featCls.scale_unit_length(para='L1')
+        """
+        flattendata = self._data.flatten().tolist()
+	if para == 'L1':
+            normdata = sum(map(lambda a: abs(a), flattendata))
+        elif para == 'L2':
+            import math
+            normdata = sum(map(lambda a: b**2, flattendata))
+            normdata = math.sqrt(normdata)
+        flattenoutput = [x/normdata if x!=0 else 0 for x in flattendata]
+        return np.reshape(np.array(flattenoutput), self._shape)
 
 
-
+         
 
 
 
