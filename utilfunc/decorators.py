ff56bbb0b39e8f73086e1b25f96eb6097f963551
hellotc <taicheng_huang@sina.cn>
2017-02-20 04:00:41 -0800

    modified timer decorator using functools

--- utilfunc/decorators.py
+++ utilfunc/decorators.py
@@ -1,12 +1,14 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode:nil -*-
 # vi: set ft=python sts=4 sw=4 et:
 
+import functools
 import time
 
 def timer(func):
     """
     timer decorator
     """    
+    @functools.wraps(func)
     def function_timer(*args, **kwargs):
         """
         Timing other function
