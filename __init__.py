8dbf5a76d179090b6859f71464087d36a0c9b2c9
hellotc <taicheng_huang@sina.cn>
2017-03-17 02:02:57 -0700

    modified __init__ to make toolbox easier to use

--- __init__.py
+++ __init__.py
@@ -1,3 +1,28 @@
-#! /usr/bin/env python
+# emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+
+"""
+ATT package is a toolbox used for analyzing nifti data
+
+Modules
+------------
+corefunc: core operation package of ATT
+algorithm: algorithm storehouse of ATT. Main completion in corefunc comes from here
+iofunc: input/output package, used for load/save files
+utilfunc: utility package, contains figure module (plotfig), file operation module (fileoperate) and decorator
+
+Usage:
+    >>> from ATT.corefunc import atlasbase
+"""
+
 
 __all__ = []
+from . import algorithm
+from . import corefunc
+from . import utilfunc
+from . import iofunc
+
+
+
+
+
---
+++ algorithm/__init__.py
@@ -0,0 +1,7 @@
+# emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+__all__ = []
+
+from . import tools
+from . import roimethod
+from . import imageopr
