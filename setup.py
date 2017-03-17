e5ce9d1e0bde7ada1e25143962dd051814c32564
hellotc <taicheng_huang@sina.cn>
2017-03-17 02:22:59 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et;
+
+from distutils.core import setup
+
+setup(name='ATT',
+      version='1.0',
+      description='Toolbox for nifti data analysis',
+      author='Taicheng Huang',
+      author_email='taicheng_huang@sina.cn',
+      url='https://github.com/helloTC/ATT',
+      packages=['algorithm', 'corefunc', 'iofunc', 'utilfunc'],
+      install_requires=['numpy', 'scipy', 'nibabel', 'six', 'sklearn', 'pandas', 'matplotlib', 'seaborn']
+      )
