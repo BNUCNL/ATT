5583954afc6afb4b3cbfd95e36cc08d43ccf7a69
helloTC <hitech9310@gmail.com>
2017-05-04 16:53:52 +0800

    modified __init__.py

--- surface/__init__.py
+++ surface/__init__.py
@@ -1,5 +1,6 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 et:
 
-from . import atlasbase
-from . import analysebase
+__all__ = []
+# from . import atlasbase
+# from . import analysebase
