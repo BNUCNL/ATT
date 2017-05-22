fix a bug that due to inconsistent sequence between python and actual computation

algorithm/tools.py
@@ -472,7 +472,7 @@ class PCorrection(object):
         Holm-Bonferroni correction method
         p(k)<=alpha/(m+1-k)
         """
-        bool_array = [e>(alpha/(self._n-i)) for i,e in enumerate(self._parray)]
+        bool_array = [e>(alpha/(self._n-(i+1))) for i,e in enumerate(self._parray)]
         return self._parray[np.argmax(bool_array)]
     
     def holm_sidak(self, alpha = 0.05):
@@ -481,7 +481,7 @@ class PCorrection(object):
         When the hypothesis tests are not negatively dependent
         p(k)<=1-(1-alpha)**(1/(m+1-k))
         """
-        bool_array = [e>(1-(1-alpha)**(1.0/(self._n-i))) for i,e in enumerate(self._parray)]
+        bool_array = [e>(1-(1-alpha)**(1.0/(self._n-(i+1)))) for i,e in enumerate(self._parray)]
         return self._parray[np.argmax(bool_array)]
 
     def fdr_bh(self, alpha = 0.05):
@@ -491,7 +491,7 @@ class PCorrection(object):
         p(k) <= alpha*k/m
         FSL by-default option
         """
-        bool_array = [e>(1.0*i*alpha/self._n) for i,e in enumerate(self._parray)]
+        bool_array = [e>(1.0*(i+1)*alpha/self._n) for i,e in enumerate(self._parray)]
         return self._parray[np.argmax(bool_array)]
 
     def fdr_bhy(self, alpha = 0.05, arb_depend = True):
@@ -506,7 +506,7 @@ class PCorrection(object):
         else:
             gamma = 0.577216
             cm = np.log(self._n) + gamma + 1.0/(2*self._n)
-        bool_array = [e>(1.0*i*alpha/(self._n*cm)) for i,e in enumerate(self._parray)] 
+        bool_array = [e>(1.0*(i+1)*alpha/(self._n*cm)) for i,e in enumerate(self._parray)] 
         return self._parray[np.argmax(bool_array)]       
 
 class NonUniformity(object):