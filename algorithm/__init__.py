3b6e1437b0021c3107f89dabaac0bb2f1d39379e
Taicheng Huang <taicheng_huang@sina.cn>
2018-08-13 00:26:36 -0700

    [DOC] Fix README.md

--- algorithm/__init__.py
+++
@@ -1,7 +0,0 @@
-# emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
-# vi: set ft=python sts=4 ts=4 sw=4 et:
-__all__ = []
-
-# from . import tools
-# from . import vol_roimethod
-# from . import imageopr
